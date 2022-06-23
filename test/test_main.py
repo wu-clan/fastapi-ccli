509b323cf539af3cf547ffc5b3dc7e01e36c9ba4
wu <jianhengwu0407@gmail.com>
2022-06-23 23:30:48 +0800

    upload the installation package

--- test/test_main.py
+++ test/test_main.py
@@ -4,7 +4,7 @@ import typer
 from prompt_toolkit.output.win32 import NoConsoleScreenBufferError as QuestionaryByTyperCliRunnerError
 from typer.testing import CliRunner
 
-from main import main
+from fastapi_ccli.main import main
 
 app = typer.Typer()
 app.command()(main)
@@ -14,6 +14,7 @@ runner = CliRunner()
 
 def test_cloner_zh():
     result = runner.invoke(app, input='n\n y\n n')
+    typer.Abort()
     assert '分析中' in result.stdout
     assert result.exit_code == 1
 
@@ -26,6 +27,7 @@ def test_cloner_zh_ic():
 
 def test_cloner_en():
     result = runner.invoke(app, input='n\n n\n n')
+    typer.Abort()
     assert 'Analyzing' in result.stdout
     assert result.exit_code == 1
 
