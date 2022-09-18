e64442bd3809afbebd3e036631029f96d1d21f59
wu <jianhengwu0407@gmail.com>
2022-09-18 13:20:34 +0800

    update test

--- test/test_main.py
+++ test/test_main.py
@@ -1,42 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import typer
+import keyboard
 from prompt_toolkit.output.win32 import NoConsoleScreenBufferError as QuestionaryByTyperCliRunnerError
 from typer.testing import CliRunner
 
-from fastapi_ccli.main import main
-
-app = typer.Typer()
-app.command()(main)
+from fastapi_ccli.CN.cloner_zh import app_zh
+from fastapi_ccli.CN.cloner_zh_form import app_zh_form
+from fastapi_ccli.EN.cloner_en import app_en
+from fastapi_ccli.EN.cloner_en_form import app_en_form
 
 runner = CliRunner()
 
 
 def test_cloner_zh():
     """ 测试非交互模式中文克隆器 """
-    result = runner.invoke(app, input='y\n n\n n\n')
-    typer.Abort()
+    result = runner.invoke(app_zh, input='n\n')
+    keyboard.send('ctrl+c')
     assert '分析中' in result.stdout
     assert result.exit_code == 1
 
 
 def test_cloner_zh_ic():
     """ 测试交互模式中文克隆器 """
-    result = runner.invoke(app, input='y\n y\n \n \n')
+    result = runner.invoke(app_zh_form, input='\n \n')
     assert QuestionaryByTyperCliRunnerError
     assert result.exit_code == 1
 
 
 def test_cloner_en():
     """ Test the English cloner in non-interactive mode """
-    result = runner.invoke(app, input='n\n n\n n\n')
-    typer.Abort()
+    result = runner.invoke(app_en, input='n\n')
+    keyboard.send('ctrl+c')
     assert 'Analyzing' in result.stdout
     assert result.exit_code == 1
 
 
 def test_cloner_en_ic():
     """ Test the English cloner in interactive mode """
-    result = runner.invoke(app, input='n\n y\n \n \n')
+    result = runner.invoke(app_en_form, input='\n \n')
     assert QuestionaryByTyperCliRunnerError
     assert result.exit_code == 1
